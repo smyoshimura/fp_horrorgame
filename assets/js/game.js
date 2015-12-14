31ff373b633bd1a11736f7f0bc39fa7f1d8e3156
Stefanie Yoshimura <syoshimura01@gmail.com>
2015-12-14 12:55:43 -0800

    grid based movement and random starting location

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,40 +1,49 @@
-var cursors, player;
+var player, rightKey, leftKey, upKey, downKey;
 
 var Game = {
-
     preload: function () {
         game.load.image('player', './assets/images/player.png');
     },
 
     create: function () {
+        //Set bounds of the world
         game.world.setBounds(0, 0, 800, 608);
 
+        //Initialize physics
         game.physics.startSystem(Phaser.Physics.Arcade);
 
-        player = game.add.sprite(32, 32, 'player');
+        //Initialize player
+        var randomX = Math.floor(Math.random() * 25) * 32,
+            randomY = Math.floor(Math.random() * 19) * 32;
+
+        player = game.add.sprite(randomX, randomY, 'player');
         game.physics.arcade.enable(player);
         player.body.collideWorldBounds = true;
 
         //Initialize keys
-        cursors = game.input.keyboard.createCursorKeys();
+        rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+
+        //Grid movement on keypress
+        rightKey.onDown.add(function () {
+            player.x += 32;
+        });
+        leftKey.onDown.add(function () {
+            player.x += -32;
+        });
+        upKey.onDown.add(function () {
+            player.y += -32;
+        });
+        downKey.onDown.add(function () {
+            player.y += 32;
+        });
 
         game.stage.backgroundColor = '#000000';
     },
 
     update: function () {
-        console.log('Update');
-        //Movement
-        if (cursors.right.isDown) {
-            player.body.x += 32;
-        }
-        else if (cursors.left.isDown) {
-            player.body.x += -32;
-        }
-        else if (cursors.up.isDown) {
-            player.body.y += -32;
-        }
-        else if (cursors.down.isDown) {
-            player.body.y += 32;
-        }
+
     }
 };
